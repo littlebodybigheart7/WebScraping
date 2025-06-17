fb4d2db6d2745e6ce3b26b2c6ef09d2ebcfd7ec8
Bruna <142622487+littlebodybigheart7@users.noreply.github.com>
2025-06-16 23:58:24 -0300

    Update main.js
    resolvendo erro da imagem n ser baixada pelo link

--- main.js
+++ main.js
@@ -1,40 +1,63 @@
 function baixarImagem(urlimg){
     fetch(urlimg)
-        .then(resp =>{
-            return resp.blob()
+        .then(resp => {
+            if (!resp.ok) {
+                throw new Error(`Erro HTTP! Status: ${resp.status} ao carregar ${urlimg}`);
+            }
+            return resp.blob();
         })
         .then(blob => {
-            const imageObjectURL = URL.createObjectURL(blob)
-            let img = document.createElement("img")
-            img.src = imageObjectURL
-            document.querySelector("#res").appendChild(img)
+            const imageObjectURL = URL.createObjectURL(blob);
+            let img = document.createElement("img");
+            img.src = imageObjectURL;
+            img.alt = "Imagem carregada"; 
+            img.style.maxWidth = "150px"; 
+            img.style.margin = "5px";
+            document.querySelector("#res").appendChild(img);
         })
-        catch(err => {
-            document.querySelector("res") = err.message;
-        })
+        .catch(err => { 
+            const resElement = document.querySelector("#res");
+            if (resElement) {
+         
+                resElement.innerHTML += `<p style="color: red;">Erro: ${err.message}</p>`;
+            } else {
+                console.error("Elemento com ID 'res' não encontrado.", err);
+            }
+        });
 }
 
 function testeImagem(){
-    const cursos = ["2/2a/4-374eb7be0bf6f36b_1526634037", "5/57/3-15dd6d3f92fe4898_1526634037","4/4e/16-4b2f253d0cbad9a2_1526634039","e/e2/13-d689107c75a973f0_1526634039","0/07/100925-081f980fb9b9fb07_1678802590","5/5c/100927-bf34d054b23a4129~1682413849"]
-    cursos.forEach(curso => {
-        baixarImagem("https://static.wikia.nocookie.net/amordoce/images/"+curso+".jpg/revision/latest/scale-to-width-down/150?cb=20210316062711")
+    const doce = ["2/2a/4-374eb7be0bf6f36b_1526634037", "5/57/3-15dd6d3f92fe4898_1526634037","4/4e/16-4b2f253d0cbad9a2_1526634039","e/e2/13-d689107c75a973f0_1526634039","0/07/100925-081f980fb9b9fb07_1678802590","5/5c/100927-bf34d054b23a4129~1682413849"]
+    doce.forEach(imagem => {
+        baixarImagem("https://static.wikia.nocookie.net/amordoce/images/"+imagem+".jpg/revision/latest/scale-to-width-down/150?cb=20210316062711" )
     })
 }
 
 function teste (){
-    fetch("https://amordoce.fandom.com/wiki/Categoria:Respostas")
+    fetch("https://amordoce.fandom.com/wiki/Categoria:Respostas" )
         .then(resp => {
             if(resp.status != 200){
-                throw new Error("Problemas no servidor")
+                throw new Error("Problemas no servidor");
             }
-            return resp.text()
+            return resp.text();
         })
         .catch(err => {
-            document.querySelector("res").innerHTML = err.message;
-        })
+            const resElement = document.querySelector("#res");
+            if (resElement) {
+                resElement.innerHTML = `<p style="color: red;">Erro na requisição: ${err.message}</p>`;
+            } else {
+                console.error("Elemento com ID 'res' não encontrado.", err);
+            }
+        });
 }
 
 function main(){
-    document.querySelector("#btnimg").addEventListener("click",testeImagem)
+    const btnImg = document.querySelector("#btnimg");
+    if (btnImg) {
+        btnImg.addEventListener("click", testeImagem);
+    } else {
+        console.error("Botão com ID 'btnimg' não encontrado.");
+    }
 }
-window.onload = main
+
+window.onload = main;
